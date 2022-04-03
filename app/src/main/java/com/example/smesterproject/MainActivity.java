41af3f18dda024f8da53e2888f8a3c6e7f1026cb
User <bitm-f19-040@superior.edu.pk>
2022-04-02 21:24:19 -0700

    login_register-dbhelper

--- app/src/main/java/com/example/smesterproject/MainActivity.java
+++ app/src/main/java/com/example/smesterproject/MainActivity.java
@@ -5,16 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView reg;
+    TextView reg , name , password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         reg=(TextView) findViewById(R.id.register);
+        name=(TextView) findViewById(R.id.id_username) ;
+        password=(TextView) findViewById(R.id.id_pass);
+
+        login=(Button) findViewById(R.id.btn_login) ;
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
         reg.setOnClickListener(new View.OnClickListener() {
             @Override
