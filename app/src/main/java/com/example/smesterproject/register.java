a9404a2b72ca91a1b604178050394fe1a78b955b
User <bitm-f19-040@superior.edu.pk>
2022-04-26 05:52:57 -0700

    reigester

--- app/src/main/java/com/example/smesterproject/register.java
+++ app/src/main/java/com/example/smesterproject/register.java
@@ -1,14 +1,31 @@
 package com.example.smesterproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class register extends AppCompatActivity {
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        register=(Button) findViewById(R.id.btn_submit) ;
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent ( register.this,home.class);
+                startActivity(intent);
+
     }
+
+    }
+        );
+}
 }
