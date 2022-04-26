32d38b326eb88d805a4f01456286f468664f2abb
User <bitm-f19-040@superior.edu.pk>
2022-04-26 05:46:05 -0700

    about

---
+++ app/src/main/java/com/example/smesterproject/aboutf.java
@@ -0,0 +1,23 @@
+package com.example.smesterproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class aboutf extends Fragment {
+
+    View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_aboutf, container, false);
+
+        view= inflater.inflate(R.layout.fragment_aboutf, container, false);
+        return view;
+    }
+}
